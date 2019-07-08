efa175d21569843ed60204d9366bb301b9a68256
JPandoras <masfar.asdipraja@gmail.com>
2019-07-08 23:10:03 +0700

    first setup spring boot

---
+++ src/main/java/app/springboot/simpleclinic/SimpleClinicApplication.java
@@ -0,0 +1,13 @@
+package app.springboot.simpleclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleClinicApplication.class, args);
+	}
+
+}
