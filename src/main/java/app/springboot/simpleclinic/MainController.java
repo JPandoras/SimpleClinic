3af48ac094dbc0df0b919aed3f808a54c664ab4c
mrpraja <masfar.asdipraja@gmail.com>
2019-07-10 09:41:29 +0700

    re-initialize repo

--- src/main/java/app/springboot/simpleclinic/MainController.java
+++ src/main/java/app/springboot/simpleclinic/MainController.java
@@ -1,21 +1,20 @@
-package app.springboot.simpleclinic.controllers;
+package app.springboot.simpleclinic;
 
 import app.springboot.simpleclinic.constants.MainConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @Controller
 public class MainController {
 
     @Value("${simple.clinic.page.title.login}")
-    private String message;
+    private String title;
 
     @GetMapping("/")
-    public String login(Map<String, Object> model) {
-        model.put("message", message);
+    public String login(Model model) {
+        model.addAttribute("title", title);
         return MainConstants.PAGE_REQUEST_LOGIN;
     }
 
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -3,9 +3,9 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html lang="en">
 <head>
-    <title>Login Page</title>
+    <title>${title}</title>
 </head>
 <body>
-    <h1>${message}</h1>
+
 </body>
 </html>
